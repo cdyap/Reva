59d144e797a36528f25ef7582ac71ee569e503d6
nelsongomez-remolona <nelsongomez.remolona@gmail.com>
2016-04-23 22:55:17 +0800

    April 23, 2016 EDIT 2
    Added Daily Count Page
    
    NOTE: First load of the page, does not work. Must refresh for animations
    ek ek to appear and work

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   root 'pages#dashboard'
 
    get 'pages/dailylitter'
-
+   get 'pages/actualdailycount'
+   
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
