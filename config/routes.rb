c22b9d81d96d367e91f2c6c1b55024a4ff66f168
nelsongomez-remolona <nelsongomez.remolona@gmail.com>
2016-04-23 23:19:05 +0800

    April 23, 2016 EDIT 3
    Added Removal Page
    
    Changed Navbar's Mortality to Removal Record
    Kept Mortality on Dashboard to keep track of deaths only
    Alignment isn't fixed yet.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
    get 'pages/dailylitter'
    get 'pages/actualdailycount'
+   get 'pages/removal'
    
 
   # The priority is based upon order of creation: first created -> highest priority.
