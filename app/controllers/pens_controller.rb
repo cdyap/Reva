75ea3966e10b732626e748cf4676f78faddb8e52
nelsongomez-remolona <nelsongomez.remolona@gmail.com>
2016-05-02 19:23:22 +0800

    May 2 2016
    Actual Daily Count (in progress)
    -error on creating building name and number field.
    -copying code from litter
    Fixed navbar (picking the last most element used to generate a
    scrollbar)

--- app/controllers/pens_controller.rb
+++ app/controllers/pens_controller.rb
@@ -1,4 +1,23 @@
 class PensController < ApplicationController
-	def index
+	def new 		
+		@pen = Pen.new
+	end 
+
+	def create 
+		@pen = Pen.new(pen_params)
+
+		if @pen.save 
+			redirect_to new_pen_path
+		else 
+			render :new
+		end 
+	end 
+
+	def index 
+		@pen = Pen.new
 	end
+
+	def pig_params 
+		params.require(:pen).permit!
+	end 
 end
