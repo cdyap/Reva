19a0958483404bc785ab570de3393bf67183f09b
cdyap <cdyap@outlook.com>
2016-05-06 13:48:17 +0800

    Autocomplete for cause of removals

--- app/controllers/cause_of_removals_controller.rb
+++ app/controllers/cause_of_removals_controller.rb
@@ -1,4 +1,5 @@
 class CauseOfRemovalsController < ApplicationController
+	autocomplete :pig, :ear_notch_number, :extra_data => [:date_of_birth, :breed, :sex], :full => true, :display_value => :autocorrect_values
 	def index
 	end
 end
