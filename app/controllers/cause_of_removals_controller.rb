c97ed6658ff9d78f1a75d99967b582cbac4e4274
nelsongomez-remolona <nelsongomez.remolona@gmail.com>
2016-05-08 20:43:22 +0800

    May 8 2016 Update 2

--- app/controllers/cause_of_removals_controller.rb
+++ app/controllers/cause_of_removals_controller.rb
@@ -1,19 +1,20 @@
 class CauseOfRemovalsController < ApplicationController
-		autocomplete :pig, :ear_notch_number, :full => true, :display_value => :autocorrect_values, scope: [:removed]
+		autocomplete :pig, :ear_notch_number, :extra_data => [:date_of_birth], :full => true, :display_value => :autocorrect_values, scope: [:removed]
 	def new 
 		@cause_of_removal = CauseOfRemoval.new 		
 	end 
 	def get_autocomplete_items(parameters)
-   		 super(parameters).where(:removed => params[:removed?])
+   		 super(parameters).where(:removed => params[:removed])
     end
 
 
 	def create 
 		@cause_of_removal = CauseOfRemoval.new(cause_of_removal_params)
 
-
 		if @cause_of_removal.save 
-			# Pig.where(pig_id: @cause_of_removal.pig_id)
+			pig = Pig.find_by(pig_id: @cause_of_removal.pig_id)
+			pig.removed = 1;
+			pig.save
 			redirect_to cause_of_removals_path
 		else 
 			render :new
